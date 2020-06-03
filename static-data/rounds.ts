9f4810485a65e29cf5665594e754eef554912697
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-06-03 09:42:56 -0400

    exercises and rounds

---
+++ static-data/rounds.ts
@@ -0,0 +1,188 @@
+export default [
+    {
+      exercisesInRound: [
+        'Jumping Jacks', 
+        'Monkey Bars', 
+        'Squat Jacks'
+      ],
+      intensity: 'medium',
+      isStarter: true,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Jumprope'
+      ],
+      intensity: 'low',
+      isStarter: true,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Monkey Bars',
+        'Lateral Steps',
+        'High Knee to Elbow Twist'
+      ],
+      intensity: 'low',
+      isStarter: true,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Jumping Jacks',
+        'Butt Kickers',
+        'Skiers'
+      ],
+      intensity: 'low',
+      isStarter: true,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Jumping Jacks',
+        'Runs',
+        'Push ups'
+      ],
+      intensity: 'medium',
+      isStarter: true,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'High Knees',
+        'Lateral Steps',
+        'High Knees'
+      ],
+      intensity: 'high',
+      isStarter: true,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Reverse Burpee to High Knees',
+        'Reverse Burpee to Jump Squat',
+        'Reverse Burpee to Jumping Knee Tuck'
+      ],
+      intensity: 'high',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Goblet Squat to High Knee',
+        'Curl and Press',
+        'Band Rows'
+      ],
+      intensity: 'medium',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Suicide Burpees',
+        'Bear Crawls',
+        'Reverse Lunges'
+      ],
+      intensity: 'high',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Squat Gap Jump Burpees',
+        'Push ups',
+        'DB Row'
+      ],
+      intensity: 'high',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Prisoner Lunge with Twist',
+        'T Push up',
+        'Band Pull Apart'
+      ],
+      intensity: 'medium',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Rotator Cuff Extension',
+        'Bear Crawls'
+      ],
+      intensity: 'medium',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Frontal Cuff Extension',
+        'Spiderman Push ups'
+      ],
+      intensity: 'medium',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Rotating Shoulder Press',
+        'KB Swing',
+        'Cross Body Row'
+      ],
+      intensity: 'medium',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Goblet Squat',
+        'Hammer Curl',
+        'Straight Arm Pulldowns'
+      ],
+      intensity: 'medium',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Glute Bridge with Chest Press',
+        'Squat to Band Pull',
+        'Push ups'
+      ],
+      intensity: 'medium',
+      isStarter: false,
+      isFinisher: false
+    },
+    {
+      exercisesInRound: [
+        'Plank Shoulder Tap and Arm Extension',
+        'Chest Fly Leg Raise',
+        'Tricep Kick Glute Bridge into Power up'
+      ],
+      intensity: 'high',
+      isStarter: false,
+      isFinisher: true
+    },
+    {
+      exercisesInRound: [
+        'Power up',
+        'Windshield Wipers',
+        'DB Rotation'
+      ],
+      intensity: 'high',
+      isStarter: false,
+      isFinisher: true
+    },
+    {
+      exercisesInRound: [
+        'DB Leg Raise',
+        'DB Knee Tuck',
+        'DB Russian Twist'
+      ],
+      intensity: 'high',
+      isStarter: false,
+      isFinisher: true
+    }
+  ]
