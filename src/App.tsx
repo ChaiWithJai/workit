ac87554dd50685e7ca4468bb7438773dcc80b0d7
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-06-08 17:03:10 -0400

    unes prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import WorkoutContainer from './workout-manager/workout-container';
 
-interface IPreferences {
-  duration: number;
-  equipment: string;
-}
-
 const App: React.FC = () => {
-  const [preferences, setPreferences] = useState<IPreferences>({duration: 0, equipment: ''})
   return (
     <div className="Workit">
       <header className="Workit-header">
