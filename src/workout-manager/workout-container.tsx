08406098384198c0cf8d573c723524e8d73db7d1
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-06-09 16:51:57 -0400

    stale closure

--- src/workout-manager/workout-container.tsx
+++ src/workout-manager/workout-container.tsx
@@ -3,7 +3,7 @@ import WorkoutExercise from './workout-exercise';
 import WorkoutWelcome from './workout-welcome';
 import WorkoutOverview from './workout-overview';
 import WorkoutPreferences from './workout-preferences';
-import {exerciseFinder, workoutBuilder, IExercise, IRound} from '../helpers/workout-builder';
+import {exerciseFinder, IExercise, IRound} from '../helpers/workout-builder';
 import staticData from '../static-data';
 import youtubeSearch from '../youtube-search-helpers/search-helper';
 import '../App.css';
@@ -18,9 +18,8 @@ export interface IPreferences {
 
 const WorkoutContainer: React.FC = () => {
     const [userFunnelIdx, setUserFunnelIdx] = useState<number>(0);
-    const [preferences, setPreferences] = useState<IPreferences>({duration: 3, equipment: ''})
     const [workout, setWorkout] = useState<IRound[]>([]);
-    const {exercises, rounds} = staticData;
+    const {exercises} = staticData;
 
     // add in youtube urls where null -- will move to middleware after backend is added
     const addYouTubeInstructionalVideo = (exercise: IExercise) => !exercise.mediaInstructions ? {...exercise, mediaInstructions: youtubeSearch(YOUTUBE_API_KEY ?? '', `how to ${exercise.name}`)} : exercise;
@@ -35,15 +34,11 @@ const WorkoutContainer: React.FC = () => {
     const renderSwitch = (idx: number) => {
         switch(idx) {
             case 0: return <WorkoutWelcome setUserFunnelIdx={setUserFunnelIdx} />;
-            case 1: return <WorkoutPreferences setUserFunnelIdx={setUserFunnelIdx} preferences={preferences} setPreferences={setPreferences} />;
+            case 1: return <WorkoutPreferences setUserFunnelIdx={setUserFunnelIdx} setWorkout={setWorkout} />;
             case 2: return <WorkoutOverview setUserFunnelIdx={setUserFunnelIdx} workout={workout} exercises={exercises} />;
             case 3: return <WorkoutExercise workoutList={workoutList} />
         }
     };
-
-    useEffect(() => {
-        setWorkout(workoutBuilder(rounds, exercises, preferences));
-    }, [preferences, exercises, rounds]);
 
 
     return (
--- src/workout-manager/workout-preferences.tsx
+++ src/workout-manager/workout-preferences.tsx
@@ -1,38 +1,51 @@
-import React from 'react';
-import {IPreferences} from './workout-container'
+import React, {useState} from 'react';
+import {IPreferences} from './workout-container';
+import {workoutBuilder, IRound} from '../helpers/workout-builder';
+import staticData from '../static-data';
 
 interface IProps {
-    preferences: IPreferences;
-    setPreferences: React.Dispatch<React.SetStateAction<IPreferences>>;
+    setWorkout: React.Dispatch<React.SetStateAction<IRound[]>>;
     setUserFunnelIdx: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const options = ['', 'dumbbell', 'band']
 
-const WorkoutPreferences: React.FC<IProps> = ({setUserFunnelIdx, setPreferences, preferences}) => {
+const WorkoutPreferences: React.FC<IProps> = ({setUserFunnelIdx, setWorkout}) => {
+    const [preferences, setPreferences] = useState<IPreferences>({duration: 3, equipment: ''})
     const {duration, equipment} = preferences;
+    const {exercises, rounds} = staticData;
+
+    const handleSubmit = () => {
+        setWorkout(workoutBuilder(rounds, exercises, preferences));
+        setUserFunnelIdx(2);
+    };
+    
     const handleOnChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        e.preventDefault();
-        setPreferences({...preferences, [e.target.name]: e.target.value});
+        const name = e.target.name ?? e.currentTarget.name
+        const value =  e.target.value ?? e.currentTarget.value;
+        setPreferences({...preferences, [name]: value});
     }
+
+    const handleOnInput = (e: React.FormEvent<HTMLInputElement | HTMLSelectElement>) => setPreferences({...preferences, [e.currentTarget.name]: e.currentTarget.value});
+
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <h2>Workout Preferences</h2>
-                <label>How many rounds do you wanna work?</label><br />
-                <input type="number" min="3" max="12" id="duration" name="duration" value={duration} onChange={(e) => handleOnChange(e)} /><br/>
-                <small><i>Rounds are 3 minutes each to replicate a boxing match.</i></small><br/><br/>
-                <label>Select what workout equipment you can use:</label><br />
-                <select id="equipment" name="equipment" value={equipment} onChange={(e) => handleOnChange(e)}>
-                    {
-                        options.map((option,idx) => {
-                        if (!option) return <option key={`option ${idx}`} value={option}>bodyweight</option>
-                        return <option key={`option ${idx}`} value={option}>{option}</option>
-                        })
-                    }
-                </select>
-                <br/><br/>
-                <button onClick={() => setUserFunnelIdx(2)}>View my workout</button>
-        </div>
+            <label>How many rounds do you wanna work?</label><br />
+            <input type="number" min="3" max="12" id="duration" name="duration" value={duration} onClick={handleOnInput} onChange={handleOnChange} /><br/>
+            <small><i>Rounds are 3 minutes each to replicate a boxing match.</i></small><br/><br/>
+            <label>Select what workout equipment you can use:</label><br />
+            <select id="equipment" name="equipment" value={equipment} onChange={handleOnChange}>
+                {
+                    options.map((option,idx) => {
+                    if (!option) return <option key={`option ${idx}`} value={option}>bodyweight</option>
+                    return <option key={`option ${idx}`} value={option}>{option}</option>
+                    })
+                }
+            </select>
+            <br/><br/>
+            <input type="submit" value="View my workout" />
+        </form>
     )
 }
 
